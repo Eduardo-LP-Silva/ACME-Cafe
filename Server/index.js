32145af8a01b08ca2ad132174d4ad57507f3b8a2
Nuno Lopes <up201605337@fe.up.pt>
2020-10-31 14:46:30 +0000

    Initial terminal app setup Small server setup

--- Server/index.js
+++ Server/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const indexRouter = require("./routes/index");
+
+app.use("/", indexRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
