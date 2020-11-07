d7c5c297e89d3655796cd11375d197ec6f947569
Eduardo Silva <eduardo__lps@hotmail.com>
2020-11-07 01:41:07 +0000

    Move order button to item page

--- Mobile/app/src/main/java/com/ejn/cmov/acmecafe/mobile/MainMenuActivity.java
+++ Mobile/app/src/main/java/com/ejn/cmov/acmecafe/mobile/MainMenuActivity.java
@@ -1,14 +1,20 @@
 package com.ejn.cmov.acmecafe.mobile;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
