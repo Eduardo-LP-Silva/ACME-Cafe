7d91f76a2875f7d1f0f7b23af7611d5efd7d6fb4
Nuno Lopes <up201605337@fe.up.pt>
2020-11-17 00:15:15 +0000

    Added pages for Admin Café Management using Pug

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const port = 8080;
 
@@ -12,14 +13,19 @@ const customerRouter = require('./routes/customer');
 const itemRouter = require('./routes/item');
 const orderRouter = require('./routes/order');
 const voucherRouter = require('./routes/voucher');
+const adminRouter = require('./routes/admin');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/customer', customerRouter);
 app.use('/item', itemRouter);
 app.use('/order', orderRouter);
 app.use('/voucher', voucherRouter);
+app.use('/admin', adminRouter);
 
 function hasEnv(envVar) {
   return envVar !== undefined && envVar !== '';
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('ACME-Cafe Server welcomes you!');
+  res.send('ACME Café Server welcomes you!');
 });
 
 module.exports = router;
